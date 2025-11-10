fc096653ddf80942bcb9bcdf7557bb4e0e1e8731
Tazim <sayeeditt@gmail.com>
2025-11-11 01:54:45 +0600

    slider added successfully

--- app/components/OfferSlider.tsx
+++ app/components/OfferSlider.tsx
@@ -29,28 +29,35 @@ const OfferSlider = () => {
         <CarouselContent className="">
           {offers.map((offer) => {
             return (
-              <CarouselItem key={offer.key} className="grid grid-cols-3 gap-2">
-                <figure className="relative w-70 overflow-hidden rounded-lg cursor-pointer group">
+              <CarouselItem
+                key={offer.key}
+                className="md:basis-1/2 lg:basis-1/3 flex items-center"
+              >
+                <figure className="relative w-80 overflow-hidden rounded-lg cursor-pointer group">
                   <Image
                     src={offer.src}
                     alt="Example"
-                    width={80}
-                    height={80}
+                    width={350}
+                    height={350}
                     className="block w-full h-auto"
+                    loading="lazy"
                   />
-                  <figcaption className="absolute left-0 right-0 bottom-0 top-0 p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-300 ease-[cubic-bezier(.22,.9,.32,1)] bg-linear-to-t from-black/60 to-transparent text-white">
-                    <h3 className="text-sm font-semibold">{offer.title}</h3>
-                    <p className="text-xs mt-1 text-white/90">{offer.desc}</p>
-                    <Link className="mt-2" href={"/view-details"}>
-                      View Details
-                    </Link>
+                  <figcaption className="absolute left-0 right-0 bottom-0 top-0 p-4 transform translate-y-full group-hover:translate-y-0 transition-transform duration-300 ease-[cubic-bezier(.22,.9,.32,1)] bg-linear-to-t from-black/80  to-black/50 text-white/70">
+                    {/* to-transparent */}
+                    <div className="flex flex-col items-center h-96">
+                      <div>
+                        <h3 className="text-sm font-semibold">{offer.title}</h3>
+                        <p className="text-xs mt-1 ">{offer.desc}</p>
+                      </div>
+                      <Link className="mt-2" href={"/view-details"}>
+                        View Details
+                      </Link>
+                    </div>
                   </figcaption>
                 </figure>
               </CarouselItem>
             );
           })}
-          {/* <CarouselItem>2</CarouselItem>
-          <CarouselItem>3</CarouselItem> */}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
